c896951629429cf56da0c38013beb61cdabcc1a5
Hang Nguyen <52890478+hangnguyen81@users.noreply.github.com>
2022-02-01 22:36:41 +0200

    Delete server directory

--- server/utils/config.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-/*eslint-env node*/
-const PORT = process.env.PORT;
-const MONGO_DB_URI = process.env.NODE_ENV === 'test'
-    ? process.env.TEST_MONGODB_URI
-    : process.env.MONGO_DB_URI;
-
-module.exports = { PORT, MONGO_DB_URI};
-
